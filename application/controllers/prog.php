4f3ac905555662eb2cfa35f8b07248e9bddc93e8
Ovidiu Pescar <ovidiupescar@gmail.com>
2017-01-16 20:51:22 +0000

    Playing with databases

---
+++ application/controllers/prog.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * @author	Ovidiu Pescar
+ * @copyright	Copyright (c) 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @since	Version 1.0.0
+ * @filesource
+ */
+
+/**
+ * Description of Programs
+ *
+ * @author Ovidiu
+ */
+class Prog extends CI_Controller {
+    public function index() {
+        $this->load->model('Programs_model');
+        $this->Programs_model->program='Daily2014';
+        $this->Programs_model->description='gaylord';
+        $this->Programs_model->save();
+        echo '<tt><pre>'.var_export($this->Programs_model, True).'</pre></tt>';
+      //  $this->load->view('programs');
+    }
+}
